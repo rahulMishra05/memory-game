dd4a5b6e8b9bf8f32bed6a979e2aca9fd8c9c37d
Rahul Mishra <rahulmishra102000@gmail.com>
2020-11-09 02:42:20 +0530

    Project setup completed

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card option
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        }
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        }
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        }
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        }
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        }
+    ]
+})
